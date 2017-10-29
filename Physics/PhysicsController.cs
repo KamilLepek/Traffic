5d9cbeac1b8dcc244b91420ea4cc856d5c5804de
KamilLepek <kamil.lepek94@gmail.com>
2017-10-29 11:01:01 +0100

    Added template for decisions and physics

---
+++ Physics/PhysicsController.cs
@@ -0,0 +1,22 @@
+﻿using Traffic.World;
+
+namespace Traffic.Physics
+{
+    public class PhysicsController
+    {
+        public Map World;
+
+        public PhysicsController(Map world)
+        {
+            this.World = world;
+        }
+
+        /// <summary>
+        /// Handles vehicles movement
+        /// </summary>
+        public void HandlePhysics()
+        {
+
+        }
+    }
+}
